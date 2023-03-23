ff8a17f3c84510c426aef0966aa4e0122e1a49cf
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-23 12:28:59 -0400

    DONE

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,48 @@
 class Movie < ActiveRecord::Base
 
+  def self.create_with_title(title)
+    movie = Movie.create(title: title)
+  end
+
+  def self.first_movie
+    self.first 
+  end
+
+  def self.last_movie
+    self.last
+  end
+
+  def self.movie_count
+    self.count
+  end
+
+  def self.find_movie_with_id(id)
+    Movie.find(id)
+  end
+  
+  def self.find_movie_with_attributes(attributes)
+    Movie.find_by(attributes)
+  end
+
+  def self.find_movies_after_2002
+    self.where('release_date > 2002')
+  end
+
+  def update_with_attributes(attr)
+    self.update(attr)
+  end
+
+  def self.update_all_titles(title)
+    Movie.update(title: title)
+  end
+
+  def self.delete_by_id(id)
+    movie = self.find(id)
+    movie.destroy
+  end
+
+  def self.delete_all_movies
+    self.destroy_all 
+  end
+
 end
